8ee58564985798cf5ad61f615a91bf7533f83b49
DTLW-a11y <3216341428@qq.com>
2025-10-14 08:16:21 +0800

    feat: 五局三胜

--- finger-guessing.c
+++ finger-guessing.c
@@ -4,6 +4,23 @@
 
 int in, com;
 char name[3][10] = { "剪刀", "石头", "布" };
+int win, lose;
+
+char checkout()
+{
+	printf("\n当前情况：\n\t胜：%d\n\t负：%d\n\n", win, lose);
+	if(win == 3)
+	{
+		printf("\n赢！\n");
+		return 'Y';
+	}
+	if(lose == 3)
+	{
+		printf("\n输！\n");
+		return 'Y';
+	}
+	return 'N';
+}
 
 char check()
 {
@@ -11,15 +28,17 @@ char check()
 	{
 		case 0:
 			printf("平局！\n\n");
-			return 'N';
+			return checkout();
 		case -1:
 		case 2:
 			printf("你输了！\n\n");
-			return 'N';
+			lose ++;
+			return checkout();
 		case 1:
 		case -2:
 			printf("你赢了！\n\n");
-			return 'Y';
+			win ++;
+			return checkout();
 	}
 	return 'N';
 }
@@ -27,6 +46,8 @@ char check()
 int main()
 {
 	srand((unsigned)time(NULL));
+	win = 0;
+	lose = 0;
 	do
 	{
 		printf("请输入你要出什么：\n（1代表剪刀，2代表石头，3代表布）\n");
@@ -43,5 +64,9 @@ int main()
 		printf("电脑出了%s\n", name[com]);
 	}
 	while(check() == 'N');
+	printf("\n回车结束游戏：");
+	char pause;
+	pause = getchar();
+	pause = getchar();
 	return 0;
 }
