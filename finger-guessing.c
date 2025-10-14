8281f776bea2e4929cbbf1404ef12ac307b38235
DTLW-a11y <3216341428@qq.com>
2025-10-14 08:41:37 +0800

    feat: 如果输入 zzz 则进入每把必赢的作弊模式

--- finger-guessing.c
+++ finger-guessing.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 int in, com;
+char stin[5], cheat;
 char name[3][10] = { "剪刀", "石头", "布" };
 int win, lose;
-
 char checkout()
 {
 	printf("\n当前情况：\n\t胜：%d\n\t负：%d\n\n", win, lose);
@@ -48,18 +49,36 @@ int main()
 	srand((unsigned)time(NULL));
 	win = 0;
 	lose = 0;
+	cheat = 'n';
 	do
 	{
 		printf("请输入你要出什么：\n（1代表剪刀，2代表石头，3代表布）\n");
-		scanf("%d", &in);
-		if(!(in == 1 || in == 2 || in == 3))
+		scanf("%s", &stin);
+		switch(strlen(stin))
+		{
+			case 1:
+			if(stin[0] == '1')in = 0;
+			else if(stin[0] == '2')in = 1;
+			else if(stin[0] == '3')in = 2;
+				break;
+			case 3:
+				if(stin[0] == 'z' && stin[1] == 'z' && stin[2] == 'z')
+				{
+					printf("\n--- 不敢说话 ---\n\n");
+					cheat = 'y';
+					in = 114;
+					continue;
+				}
+				break;
+			default:
+				in = 114;
+		}
+		if(!(in == 0 || in == 1 || in == 2))
 		{
 			printf("你在输入什么？？？\n");
-			in = 114;
 			continue;
 		}
-		in --;
-		com = rand() % 3;
+		com = (cheat == 'n') ? (rand() % 3) : (in - 1);
 		printf("\n你出的是%s\n", name[in]);
 		printf("电脑出了%s\n", name[com]);
 	}
